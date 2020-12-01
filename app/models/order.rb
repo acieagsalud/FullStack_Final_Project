ebc28fce901788dc00bed212ae5191ccf12ae0ec
acieagsalud <aciejohn@gmail.com>
2020-11-30 23:58:53 -0600

    Added model validations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,5 @@
 class Order < ApplicationRecord
   belongs_to :user
+  validates :total, :subtotal, :gst, :pst, numericality: true
+  validates :order_date, :total, :subtotal, :gst, :pst, :status, :stripe_id, presence: true
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,6 @@ class Product < ApplicationRecord
   belongs_to :manufacturer
   belongs_to :subcategory
 
-  validates :name, :price, :stock_qty, presence: true
+  validates :name, :price, :stock_qty, :description, presence: true
+  validates :price, :stock_qty, :sale_price, numericality: true
 end
