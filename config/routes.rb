84d1d4db637eadf0485eb5d999982cd683a399f9
acieagsalud <aciejohn@gmail.com>
2020-12-01 22:42:37 -0600

    Altered route to use alias in the Signup button link

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: %i[new create]
-  get "login", to: "sessions#new"
+  get "login", to: "sessions#new", as: "login"
   post "login", to: "sessions#create"
   get "welcome", to: "sessions#welcome"
   get "authorized", to: "sessions#page_requires_login"
